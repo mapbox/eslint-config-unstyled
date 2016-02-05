c0d7812d66cf57a45ad1fd522e7d4bb70a1a41c3
Tom MacWright <tom@macwright.org>
2016-02-05 18:40:15 -0500

    First

---
+++ index.js
@@ -0,0 +1,11 @@
+module.exports = {
+  rules: {
+    "no-invalid-regexp": [2],
+    "no-obj-calls": [2],
+    "valid-typeof": [2],
+    "no-undef": [2],
+    "no-fallthrough": [2],
+    "no-invalid-this": [2],
+    "no-native-reassign": [2]
+  }
+};
